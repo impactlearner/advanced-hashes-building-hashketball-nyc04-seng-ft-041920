b38cf8dba5e38f0c5cd63670d3eb773e2735d332
impact learner <time.darden@gmail.com>
2020-05-10 23:51:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,15 +1,6 @@
 # Write your code here!
 def game_hash
-{
-  home:{
-    team_name: "Brooklyn Nets"
-    team_colors: ["Black", "White"],
-    players: {
-      "Alan Anderson" => {
-        number
-      }
-    }
-
-  }}
-
+{home: {},
+team: {}
+}
 end
