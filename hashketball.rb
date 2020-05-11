3f0db9f1a23d3dbc5f7fbbb0dc2a5c2dec551b49
impact learner <time.darden@gmail.com>
2020-05-11 00:23:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,16 +15,16 @@ def game_hash
         blocks: 1,
         slam_dunks: 1
       },
-      "Reggie Evans" => {
-        number: 30,
-        shoe: 14,
-        points: 12,
-        rebounds: 12,
-        assists: 12,
-        steals: 12,
-        blocks: 12,
-        slam_dunks: 7
-      },
+      # "Reggie Evans" => {
+      #   number: 30,
+      #   shoe: 14,
+      #   points: 12,
+      #   rebounds: 12,
+      #   assists: 12,
+      #   steals: 12,
+      #   blocks: 12,
+      #   slam_dunks: 7
+      # },
       # "Brook Lopez" => {
       #   number: 11,
       #   shoe: 17,
