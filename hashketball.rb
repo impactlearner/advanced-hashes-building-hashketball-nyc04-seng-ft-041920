50e5283cf3dbe1880faa87afbb84af25ed6dc0d3
impact learner <time.darden@gmail.com>
2020-05-11 00:21:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -25,15 +25,15 @@ def game_hash
         blocks: 12,
         slam_dunks: 7
       },
-      "Brook Lopez" => {
-        number: 11,
-        shoe: 17,
-        points: 17,
-        rebounds: 19,
-        assists: 10,
-        steals: 3,
-        blocks: 1,
-        slam_dunks: 15
+      # "Brook Lopez" => {
+      #   number: 11,
+      #   shoe: 17,
+      #   points: 17,
+      #   rebounds: 19,
+      #   assists: 10,
+      #   steals: 3,
+      #   blocks: 1,
+      #   slam_dunks: 15
       },
       # "Mason Plumlee" => {
       #   number: 1,
